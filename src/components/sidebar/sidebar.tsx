70d1fe04393599356c7868eaf0f96b12e3ca7f72
ajaySankapal <ajay.sankapal@tensorgo.com>
2024-06-19 21:12:23 +0530

    landing page updated

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { getFolders, getUserSubscriptionStatus } from "@/lib/supabase/queries";
+import { twMerge } from "tailwind-merge";
+import {
+  getCollaboratingWorkspaces,
+  getFolders,
+  getPrivateWorkspaces,
+  getSharedWorkspaces,
+  getUserSubscriptionStatus,
+} from "@/lib/supabase/queries";
 import { error } from "console";
 import { redirect } from "next/navigation";
+import WorkspaceDropdown from "./workspace-dropdown";
 interface SidebarProps {
   params: { workspaceId: string };
   className?: string;
@@ -27,7 +35,23 @@ const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
   //error
   if (subscriptionError || foldersError) redirect("/dashboard");
 
-  return <div>Sidebar</div>;
+  const [privateWorkspaces, collaboratingWorkspaces, sharedWorkspaces] =
+    await Promise.all([
+      getPrivateWorkspaces(user.id),
+      getCollaboratingWorkspaces(user.id),
+      getSharedWorkspaces(user.id),
+    ]);
+
+  return (
+    <aside
+      className={twMerge(
+        "hidden sm:flex sm:flex-col w-[280px] shrink-0 p-4 md:gap-4 !justify-between",
+        className
+      )}
+    >
+      <WorkspaceDropdown />
+    </aside>
+  );
 };
 
 export default Sidebar;
---
+++ src/components/sidebar/workspace-dropdown.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WorkspaceDropdown = () => {
+  return <div>WorkspaceDropdown</div>;
+};
+
+export default WorkspaceDropdown;
